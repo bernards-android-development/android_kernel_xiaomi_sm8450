cb862ca77245d95017e21e69f093568ad2955065
Juhyung Park <qkrwngud825@gmail.com>
2023-02-14 22:03:14 +0900

    msm: kgsl: disable snapshot, coresight and trace
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -750,8 +750,6 @@ static int dispatcher_context_sendcmds(struct adreno_device *adreno_dev,
 		timestamp = drawobj->timestamp;
 		cmdobj = CMDOBJ(drawobj);
 		context = drawobj->context;
-		trace_adreno_cmdbatch_ready(context->id, context->priority,
-			drawobj->timestamp, cmdobj->requeue_cnt);
 		ret = sendcmd(adreno_dev, cmdobj);
 
 		/*
@@ -1914,17 +1912,18 @@ replay:
 static void do_header_and_snapshot(struct kgsl_device *device, int fault,
 		struct adreno_ringbuffer *rb, struct kgsl_drawobj_cmd *cmdobj)
 {
-	struct kgsl_drawobj *drawobj = DRAWOBJ(cmdobj);
-
 	/* Always dump the snapshot on a non-drawobj failure */
 	if (cmdobj == NULL) {
 		adreno_fault_header(device, rb, NULL, fault);
+<<<<<<< HEAD
 
 		/* GMU snapshot will also pull a full device snapshot */
 		if (fault & ADRENO_GMU_FAULT)
 			gmu_core_fault_snapshot(device);
 		else
 			kgsl_device_snapshot(device, NULL, NULL, false);
+=======
+>>>>>>> c7ecd7c35ae2 (msm: kgsl: disable snapshot, coresight and trace)
 		return;
 	}
 
@@ -1934,10 +1933,6 @@ static void do_header_and_snapshot(struct kgsl_device *device, int fault,
 
 	/* Print the fault header */
 	adreno_fault_header(device, rb, cmdobj, fault);
-
-	if (!(drawobj->context->flags & KGSL_CONTEXT_NO_SNAPSHOT))
-		kgsl_device_snapshot(device, drawobj->context, NULL,
-					fault & ADRENO_GMU_FAULT);
 }
 
 static int dispatcher_do_fault(struct adreno_device *adreno_dev)
@@ -2218,6 +2213,15 @@ static void retire_cmdobj(struct adreno_device *adreno_dev,
 		atomic64_inc(&context->proc_priv->frame_count);
 		atomic_inc(&context->proc_priv->period->frames);
 	}
+
+	log_kgsl_cmdbatch_retired_event(context->id, drawobj->timestamp,
+		context->priority, drawobj->flags, start, end);
+
+	drawctxt->submit_retire_ticks[drawctxt->ticks_index] =
+		end - cmdobj->submit_ticks;
+
+	drawctxt->ticks_index = (drawctxt->ticks_index + 1) %
+		SUBMIT_RETIRE_TICKS_SIZE;
 
 	kgsl_drawobj_destroy(drawobj);
 }
--- drivers/gpu/msm/adreno_snapshot.h
+++ drivers/gpu/msm/adreno_snapshot.h
@@ -8,6 +8,8 @@
 
 #include "kgsl_snapshot.h"
 
+#if 0
+
 #define CP_CRASH_DUMPER_TIMEOUT 500
 
 #define DEBUG_SECTION_SZ(_dwords) (((_dwords) * sizeof(unsigned int)) \
@@ -119,6 +121,7 @@ void adreno_parse_ib_lpac(struct kgsl_device *device,
  */
 size_t adreno_snapshot_global(struct kgsl_device *device, u8 *buf,
 	size_t remain, void *priv);
+<<<<<<< HEAD
 
 /**
  * adreno_snapshot_dump_all_ibs - To dump all ibs from ringbuffer
@@ -132,4 +135,7 @@ void adreno_snapshot_dump_all_ibs(struct kgsl_device *device,
 			unsigned int *rbptr,
 			struct kgsl_snapshot *snapshot);
 
+=======
+#endif
+>>>>>>> c7ecd7c35ae2 (msm: kgsl: disable snapshot, coresight and trace)
 #endif /*__ADRENO_SNAPSHOT_H */
